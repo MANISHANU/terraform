152d256261b2e1f92efb47f011802e53ab140c6f
Manisha <manishamaidam169@gmail.com.com>
2024-05-31 13:12:39 +0530

    tf

--- ec2/variables/ec2.tf
+++ ec2/variables/ec2.tf
@@ -3,10 +3,7 @@ resource "aws_instance" "db" {
     ami = var.image_id
     vpc_security_group_ids = [aws_security_group.allow_ssh.id]
     instance_type = var.instance_type
-
-    tags = {
-        Name = var.tags
-    }
+    tags =  var.tags
 }
 
 resource "aws_security_group" "allow_ssh" {
